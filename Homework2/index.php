b57b96daa3b28596324460fe5de0e06f77e130ef
jperera01 <thebluewave2@yahoo.com>
2022-09-20 21:42:03 -0400

    Required Homework 2 Attributes

---
+++ Homework2/index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>PHP Test</title>
+</head>
+<body>
+
+</body>
+</html>
+
+<?php echo '<p>Hello! The current server  time is: </p>'; ?>
+<?php echo  date("m-d-Y  h:i:ss") ; ?>
+
+<?php echo '<p> new line test</p>'; ?>
+<?php echo '<p> test test test </p>'; ?>
+
+<?php echo '<p> Jemes Perera  </p>'; ?>
